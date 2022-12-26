1681006a7f4cc79b83295f302339692bfecaf11e
1hect0r1 <74503013+1hect0r1@users.noreply.github.com>
2022-12-26 19:05:21 +0000

    Update keymap.c

--- pancake_handwired/QMK firmware/keymaps/default/keymap.c
+++ pancake_handwired/QMK firmware/keymaps/default/keymap.c
@@ -3,9 +3,8 @@
 #define _LOWER 1
 #define _RAISE 2
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
+    
     [_QWERTY] = LAYOUT_planck_grid( /* QWERTY */
        KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
        KC_CAPS, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
